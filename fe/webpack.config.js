1f12f35c2db8fc682fbd69540c817e1623819ecc
botagar <botagar@gmail.com>
2023-02-06 19:41:42 +1000

    Initial Commit

---
+++ fe/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+const DIST = path.resolve(__dirname, 'dist')
+
+const config = {
+  entry: "./src/index.tsx",
+  output: {
+    path: DIST,
+    publicPath: 'auto',
+    filename: '[name].bundle.[fullhash:8].js',
+    chunkFilename: '[name].bundle.[contenthash:8].js',
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    static: [
+      DIST
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: "index.html",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
